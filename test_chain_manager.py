9101da96da92d32daf4fb9d5383244dfbd018058
Monika Osiak <m.osiak46@gmail.com>
2021-11-19 23:06:56 +0100

    Add dummy unit tests to be finished later

---
+++ test_chain_manager.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from chain_manager import ChainManager
+
+class TestChainManager(TestCase):
+    def test_always_passes(self):
+        ChainManager()
+        self.assertTrue(True)
