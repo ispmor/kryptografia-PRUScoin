b0c1b74c7b6c238320676de774105af7e18e352e
Monika Osiak <m.osiak46@gmail.com>
2021-11-19 23:06:36 +0100

    Add a dummy version of User class

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self):
+        self.hash = None # wartość przypisujemy dopiero potem
+        self.wallet = dict()
+
+    def checkout(self):
+        pass
+
+    def validate_blockchain(self):
+        pass
